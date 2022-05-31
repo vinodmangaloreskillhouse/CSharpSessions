07e4d769ac8c8eeda69ecfb02dad9ef53143b94c
Rohan <Rohan@DESKTOP-G9G11CU>
2022-05-31 16:01:35 +0530

    Commiting ObjectOriented Project

---
+++ CSharpExercises/Session01/Rohan/ObjectOrientedProject/ObjectOrientedProject/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProject
+{
+    internal class Box
+    {
+        public Box()
+        {
+            //Constructor
+            //Can execute any logic when object gets created
+            Console.WriteLine("Object Created");
+        }
+        public double Height { get; set; }
+
+        public double Width { get; set; }
+
+        public double Breadth { get; set; }
+    }
+}
