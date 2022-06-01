7eb9b1e98102e38e0e62836c5f9fa8e3de59f885
Aishwarya <HP@LAPTOP-LO197HHG>
2022-06-02 00:33:15 +0530

    I am commiting Employee Task.

---
+++ CSharpExercises/Session01/Aishwarya/Employee/Employee/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Employee;
+
+Employees Employee1 = new Employees("Thomas",56, "Chief Technical Officer","DataMart");
+Console.WriteLine("Command: Work or Stand Up");
+string v = Console.ReadLine();
+Employee1.Command(v);
+
+Employees Employee2 = new Employees("Pinto", 60, "Chief Operating Officer", "QMart");
+Console.WriteLine("\nCommand: Work or Stand Up");
+string s = Console.ReadLine();
+Employee2.Command(s);
+
+
+
+
