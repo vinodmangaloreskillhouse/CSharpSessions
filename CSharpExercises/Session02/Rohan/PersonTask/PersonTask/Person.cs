08aef1dff54620646887c5d9c103bafea65d68ca
Rohan <Rohan@DESKTOP-G9G11CU>
2022-06-05 23:14:59 +0530

    Commiting Person Task Program.

---
+++ CSharpExercises/Session02/Rohan/PersonTask/PersonTask/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonTask
+{
+    internal class Person
+    {
+        int Age;
+        string Name,Country;
+        public Person(string name,int age,string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public void Breakfast(string breakfastItem)
+        {
+            string BreakfastItem= breakfastItem;
+            Console.WriteLine("\nHi,I am "+this.Name+" I am from "+this.Country+" I had " +BreakfastItem+" for breakfast");
+        }
+
+        public string Lunch(string lunchItem)
+        {
+            string LunchItem = lunchItem;
+            Console.WriteLine("I had "+lunchItem+" for lunch");
+            return null;
+        }
+
+        public string Dinner(string dinnerItem)
+        {
+            string DinnerItem=dinnerItem;
+            //Console.WriteLine("I had "+DinnerItem+" for dinner");
+            return DinnerItem;
+        }
+    }
+}
