960d2d87aa38e3153f8fb1882a79700f2fe67c7b
karthikmsk <karthikms207@gmail.com>
2022-06-05 10:25:59 +0530

    Commiting new task

---
+++ CSharpExercises/Session01/KarthikMS/ConsoleAppPeople/ConsoleAppPeople/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPeople
+{
+    internal class Person
+    {
+      public  string Name { get; set; }
+      public int Age { get; set; }
+      public string Country { get; set; }
+      public string BreakfastFood { get; set; }
+      public string LunchFood { get; set; }
+      public string DinnerFood { get; set; }
+      public void Breakfats()
+        {
+            Console.WriteLine("Hi! I'm " +this.Name +" I'm "+this.Age +" year old and "+" I'm from "+this.Country);
+            Console.WriteLine("I had breakfast and I ate " +this.BreakfastFood);
+        }
+       public void Lunch()
+        {
+            Console.WriteLine("I had lunch and I ate " +this.LunchFood);
+
+        }
+        public void Dinner()
+        {
+            Console.WriteLine("I had dinner and I ate " +this.DinnerFood);
+
+        }
+    }
+}
---
+++ CSharpExercises/Session01/KarthikMS/ConsoleAppPeople/ConsoleAppPeople/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppPeople;
+
+Person p1 = new Person();
+p1.Name = "Lokesh";
+p1.Age = 45;
+p1.Country = "India";
+p1.BreakfastFood = "Roti";
+p1.LunchFood = "South Indian Thali";
+p1.DinnerFood = "Chapathi";
+p1.Breakfats();
+p1.Lunch();
+p1.Dinner();
+
+Person p2 = new Person();
+p2.Name = "Tom";
+p2.Age = 50;
+p2.Country = "USA";
+p2.BreakfastFood = "Salad";
+p2.LunchFood = "Burger";
+p2.DinnerFood = "Egg Sandwich";
+p2.Breakfats();
+p2.Lunch();
+p2.Dinner();
+
