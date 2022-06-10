94d5f99002f81bb988c0b32de56b84b41f2d3795
Aishwarya <HP@LAPTOP-LO197HHG>
2022-06-10 22:28:22 +0530

    Commiting PhoneTask.

---
+++ CSharpExercises/Session04/Aishwarya/PhoneTask/PhoneTask/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PhoneTask;
+MobilePhone phone=new MobilePhone("Snapdragon", "4G", 60, 100, "Android 13", "MediaTek", "QuadCore", "Mi");
+phone.TurnOn();
+phone.TurnOFF();
