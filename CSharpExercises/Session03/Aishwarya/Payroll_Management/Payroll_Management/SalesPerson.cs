326fdbc798461e4c4ecf20d75cef3d590daa3b34
Aishwarya <HP@LAPTOP-LO197HHG>
2022-06-18 22:06:18 +0530

    commiting PayrollManagement

---
+++ CSharpExercises/Session03/Aishwarya/Payroll_Management/Payroll_Management/SalesPerson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_Management
+{
+    internal class SalesPerson:Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("  I am" + this.Name + " now I am travelling");
+        }
+        public override void Bonus()
+        {
+            Console.WriteLine("Bonus amopunt is 9000");
+        }
+        public override double getSalary()
+        {
+            return salary;
+        }
+
+
+    }
+}
---
+++ CSharpExercises/Session03/Aishwarya/Payroll_Management/Payroll_Management/SoftwareEngineer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_Management
+{
+    internal class SoftwareEngineer:Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("  I am"+this.Name+" now  I am coding");
+        }
+        public override void Bonus()
+        {
+            Console.WriteLine("Bonus amopunt is 6000");
+        }
+        public override double getSalary()
+        {
+            return salary;
+        }
+
+
+    }
+}
