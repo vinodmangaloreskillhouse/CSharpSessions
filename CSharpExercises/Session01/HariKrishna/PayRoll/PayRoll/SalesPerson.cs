c07b9af7718654f5c8e5c51a1fb5cc08e0275cb7
Harikrishna006 <srinihari86@gmail.com>
2022-06-18 22:23:38 +0530

    I'm Checking the task.

---
+++ CSharpExercises/Session01/HariKrishna/PayRoll/PayRoll/SalesPerson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRoll
+{
+    internal class SalesPerson : Employee
+    {
+        public override void basesalary()
+        {
+            Console.WriteLine("The base salary for Sales Person is Rs.600000");
+        }
+
+        public override void bonus()
+        {
+            Console.WriteLine("The bonus for the Sales Person is Rs.10000");
+        }
+
+        public override void work()
+        {
+            Console.WriteLine("I'm Travelling");
+        }
+    }
+}
