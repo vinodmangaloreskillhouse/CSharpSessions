5bd5cbab10a0f14f5340439e0f2565b111e5f870
karthikmsk <karthikms207@gmail.com>
2022-06-18 22:21:48 +0530

    Commiting pay roll task

---
+++ CSharpExercises/Session03/KarthikMS/PayRoll/Organization/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PayRollTask;
+Salesman sm = new Salesman("Anush","4SN100",40000,5000);
+Salesman.GetOrganizationName();
+Salesman.walk();
+sm.work();
+
+HR hr = new HR("Vinay", "4MN100", 60000, 5000);
+HR.GetOrganizationName();
+HR.walk();
+hr.work();
+
+SoftwareEngineer se = new SoftwareEngineer("Dhanush", "4NN100", 50000, 6000);
+SoftwareEngineer.GetOrganizationName();
+SoftwareEngineer.walk();
+se.work();
