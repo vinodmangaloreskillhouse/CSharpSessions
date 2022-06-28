d789a6749b6140a6302ba7297661d5696fbd8d4a
karthikmsk <karthikms207@gmail.com>
2022-06-28 22:50:45 +0530

    User task Complited

---
+++ CSharpExercises/Session11/KarthikMS/UserTask/UserTask/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTask
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime LastLoggedIn { get; set; }
+    }
+}
