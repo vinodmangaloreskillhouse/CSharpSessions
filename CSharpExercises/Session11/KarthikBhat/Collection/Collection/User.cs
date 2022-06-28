dd1ff70aa0429d00492d481c1ebbbdb7d79b07a4
Karthik Bhat <bhatkarthik279@gmail.com>
2022-06-28 22:08:42 +0530

    list task completed

---
+++ CSharpExercises/Session11/KarthikBhat/Collection/Collection/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime LastLoggedIn { get; set; }  
+
+    }
+}
