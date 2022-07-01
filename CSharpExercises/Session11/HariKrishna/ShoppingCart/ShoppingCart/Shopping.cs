04e476dab2344dee88917c17186d03a7d6afb984
Harikrishna006 <srinihari86@gmail.com>
2022-07-01 19:52:59 +0530

    ShoppingCart Task done.

---
+++ CSharpExercises/Session11/HariKrishna/ShoppingCart/ShoppingCart/Shopping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    internal class Shopping
+    {
+        List<string> cart = new List<string>();
+
+        public void AddtoCart(string item)
+        {
+            cart.Add(item);
+        }
+
+        public void RemoveFromCart(string item)
+        {
+            var itemIndex = cart.IndexOf(item);
+            cart.RemoveAt(itemIndex);
+        }
+
+        public void Show()
+        {
+            foreach (var item in cart)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
