f771e095cab8a53cc8684d378b547795c169255a
Rohan <Rohan@DESKTOP-G9G11CU>
2022-07-01 16:59:28 +0530

    Committing ShoppingCart Task.

---
+++ CSharpExercises/Session11/Rohan/ShoppingTask/ShoppingTask/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingTask
+{
+    internal class ShoppingCart
+    {
+        public List<string> items = new List<string>();
+
+        public void AddToCart(string item)
+        {
+            items.Add(item);
+        }
+
+        public void RemoveFromCart(string item)
+        {
+            var itemIndex = items.IndexOf(item);
+            items.RemoveAt(itemIndex);
+        }
+
+    }
+}
