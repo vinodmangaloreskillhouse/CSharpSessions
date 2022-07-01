77fcdab6381c89ec37bdc0db30082a742564b55d
karthikmsk <karthikms207@gmail.com>
2022-07-01 17:14:51 +0530

    Commiting the Shopping Task

---
+++ CSharpExercises/Session11/KarthikMS/Shopping/Shopping/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Shopping;
+
+ShoppingCart sc=new ShoppingCart();
+Console.WriteLine("Before removing the items:");
+sc.AddToCart("Book");
+sc.AddToCart("Soap");
+sc.AddToCart("Bottle");
+sc.AddToCart("Shirt");
+sc.Display();
+
+sc.RemoveFromCart("Book");
+sc.RemoveFromCart("Shirt");
+Console.WriteLine("After removing the items:");
+sc.Display();
+
---
+++ CSharpExercises/Session11/KarthikMS/Shopping/Shopping/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    internal class ShoppingCart
+    {
+     
+        List<String>Cart=new List<string>();
+        public String Items { get; set; }
+        public void AddToCart(String Items)
+        {
+            Cart.Add(Items);
+    
+        }
+        public void RemoveFromCart(String Items)
+        {
+            var ItemIndex = Cart.IndexOf(Items);
+            Cart.RemoveAt(ItemIndex);
+        }
+        public void Display()
+        {
+            foreach (var Item in Cart)
+            {
+                Console.WriteLine(Item);
+            }
+        } 
+    }
+
+}
