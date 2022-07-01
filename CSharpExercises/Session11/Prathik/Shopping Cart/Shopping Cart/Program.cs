fec79a22fae03e50b3f3226bd058e31e6ec40d18
Pratik14naik <pnaik090@gmail.com>
2022-07-01 19:37:55 +0530

    Shopping Cart Task Completed

---
+++ CSharpExercises/Session11/Prathik/Shopping Cart/Shopping Cart/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Shopping;
+ShoppingCart cart = new ShoppingCart();
+cart.AddToCart("Book");
+cart.AddToCart("Pencil");
+cart.RemoveFromCart("Book");
+cart.AddToCart("Toys");
+cart.AddToCart("Bread");
+cart.RemoveFromCart("Toys");
+cart.Display();
---
+++ CSharpExercises/Session11/Prathik/Shopping Cart/Shopping Cart/Shopping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    public class ShoppingCart
+    {
+        List<string> Cart = new List<string>();
+        public void AddToCart (string item)
+        {
+            Cart.Add(item);
+        }
+        public void RemoveFromCart(string item)
+        {
+            var itemIndex = Cart.IndexOf(item);
+            Cart.RemoveAt(itemIndex);
+
+        }
+        public void Display()
+        {
+            foreach (var item in Cart)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
