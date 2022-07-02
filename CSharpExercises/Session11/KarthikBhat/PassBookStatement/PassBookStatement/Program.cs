5d30717829077f1567c0479a95a2a73b65a64822
Karthik Bhat <bhatkarthik279@gmail.com>
2022-07-02 17:22:17 +0530

    Commiting PassBook Statement task

---
+++ CSharpExercises/Session11/KarthikBhat/PassBookStatement/PassBookStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PassBookStatement;
+Statement statement1 = new Statement();
+statement1.Title = "Adding";
+statement1.Description = "Goutham";
+statement1.Date = DateTime.Now;
+statement1.Amount = 1000;
+statement1.IsAdd = true;
+statement1.IsDeduction = false;
+
+Statement statement2=new Statement();
+statement2.Title = "Deduction";
+statement2.Description = "karthik";
+statement2.Date = DateTime.Now;
+statement2.Amount = 200000;
+statement2.IsAdd =false;
+statement2.IsDeduction = true;
+
+PassBook pass = new PassBook();
+pass.AddEntry(statement1);
+pass.AddEntry(statement2);
+pass.PrintEntries(statement1);
+pass.PrintEntries(statement2);
+
+Console.WriteLine("Total number of Entry are: "+pass.Totalentry());
---
+++ CSharpExercises/Session11/KarthikBhat/PassBookStatement/PassBookStatement/Statement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassBookStatement
+{
+    internal class Statement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public float Amount { get; set; }
+        public bool IsAdd { get; set; }
+        public bool IsDeduction { get; set; }
+    }
+}
