15c81d862a3dbf3f36fb76da585aba30b4d148ab
Pratik14naik <pnaik090@gmail.com>
2022-07-02 18:25:42 +0530

    Commiting NameCollection Task

---
+++ CSharpExercises/Session11/Prathik/NameCollection/NameCollection/NameCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedCollection
+{
+    internal class NameCollection
+    {
+        List<string> nameList = new List<string>();
+        public void AddName(string name)
+        {
+            nameList.Add(name);
+        }
+        public void ReverseClone()
+        {
+            var newList = new List<string>();
+            for (int i =nameList.Count() -1; i >= 0; i--)
+            {
+                newList.Add(nameList[i]);
+            }
+            foreach (string name in newList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
