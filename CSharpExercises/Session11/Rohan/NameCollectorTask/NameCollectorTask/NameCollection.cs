45c6a7bfbd691695a9df2e21387214e2bb1d15f2
Rohan <Rohan@DESKTOP-G9G11CU>
2022-07-02 17:58:52 +0530

    Commiting NameCollection Task.

---
+++ CSharpExercises/Session11/Rohan/NameCollectorTask/NameCollectorTask/NameCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameCollectorTask
+{
+    internal class NameCollection
+    {
+        public List<string> nameList = new List<string>();
+        public void AddNames(string name)
+        {
+            nameList.Add(name);
+        }
+
+        public List<string> ReverseClone()
+        {
+            var newList = new List<string>();
+            for (int i = nameList.Count() - 1; i >= 0; i--)
+                newList.Add(nameList[i]);
+            /*foreach (string name in nameList)
+                newList.Add(name);
+            newList.Reverse();*/
+            return newList;
+        }
+    }
+}
---
+++ CSharpExercises/Session11/Rohan/NameCollectorTask/NameCollectorTask/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NameCollectorTask;
+
+NameCollection names = new NameCollection();
+names.AddNames("Rohan");
+names.AddNames("Vinod");
+names.AddNames("Jaideep");
+
+foreach (var name in names.nameList)
+    Console.WriteLine(name);
+
+var reversedList = names.ReverseClone();
+Console.WriteLine("\nReversed List");
+foreach (var name in reversedList)
+    Console.WriteLine(name);
