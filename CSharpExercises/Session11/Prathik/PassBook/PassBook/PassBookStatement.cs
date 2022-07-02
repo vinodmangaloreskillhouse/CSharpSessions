1375bef517d9e9c42d2c5eda3e6fb710bee944fd
Pratik14naik <pnaik090@gmail.com>
2022-07-02 19:26:58 +0530

    Commiting PassBook Task

---
+++ CSharpExercises/Session11/Prathik/PassBook/PassBook/PassBookStatement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassBookDetails
+{
+    internal class PassBook
+    {
+        List<Statement> statements = new List<Statement>();
+        public void AddEntry(Statement statement)
+        {
+            statements.Add(statement);
+        }
+        public void PrintEntries(Statement statement)
+        {
+            Console.WriteLine("\nTitle: " + statement.Title + "\nStatement: " + statement.Description + "\nDate: " + statement.Date + "\nAmount:" + statement.Amount + "\nIsAdd: " + statement.IsAdd + "\nIsDeducted: " + statement.IsDeduction);
+        }
+        public int TotalEntries()
+        {
+           return statements.Count;
+        }
+            
+    }
+}
---
+++ CSharpExercises/Session11/Prathik/PassBook/PassBook/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PassBookDetails;
+Statement statement1 = new Statement();
+statement1.Title = "Credit";
+statement1.Description = "Deposit from Credit Card Processor ";
+statement1.Date = DateTime.Now;
+statement1.Amount = 15000;
+statement1.IsAdd = true;
+statement1.IsDeduction = false;
+
+Statement statement2 = new Statement();
+statement2.Title = "Debit";
+statement2.Description = "Bill Paid";
+statement2.Date = DateTime.Now;
+statement2.Amount = 5000;
+statement2.IsAdd = false;
+statement2.IsDeduction = true;
+
+PassBook book = new PassBook();
+book.AddEntry(statement1);
+book.AddEntry(statement2);
+book.PrintEntries(statement1);
+book.PrintEntries(statement2);
+
+Console.WriteLine("Total Entries are: " + book.TotalEntries());
