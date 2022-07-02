33ca38154f15e2dd0d023fe0b4be72c31d1cf9eb
Rohan <Rohan@DESKTOP-G9G11CU>
2022-07-02 17:59:31 +0530

    Commiting PassBook Task

---
+++ CSharpExercises/Session11/Rohan/PassbookTask/PassbookTask/PassBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassbookTask
+{
+    internal class PassBook
+    {
+        List<Statement> statements = new List<Statement>();
+
+        public void AddEntry()
+        {
+            statements.Add(new Statement { Title ="Food", Description = "Lunch", Date = DateTime.Now, Amount = 455.78, isAdd = false, isDeducted = true });
+            statements.Add(new Statement { Title = "Salary", Description = "Work", Date = DateTime.Now, Amount = 45756.9, isAdd = true, isDeducted = false });
+            statements.Add(new Statement { Title = "Grocery", Description = "House", Date = DateTime.Now, Amount = 1109.8, isAdd = false, isDeducted = true });
+
+        }
+        public void PrintEntries()
+        {
+            foreach (Statement s in statements)
+            {
+                Console.WriteLine("Title: {0} Description: {1} Date: {2} Amount: {3} IsAdded: {4} IsDeducted: {5} ", s.Title, s.Description, s.Date, s.Amount, s.isAdd,s.isDeducted);
+            }
+        }
+        public void TotalEntries()
+        {
+            Console.WriteLine("\nTotal Entries: {0}",statements.Count);
+        }
+    }
+}
