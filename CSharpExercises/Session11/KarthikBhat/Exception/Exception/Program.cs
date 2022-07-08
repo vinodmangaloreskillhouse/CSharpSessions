7cdc43438b91a21270721c565de0d004efaa7c57
Karthik Bhat <bhatkarthik279@gmail.com>
2022-07-08 18:37:37 +0530

    Exception task

---
+++ CSharpExercises/Session11/KarthikBhat/Exception/Exception/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] sarray = { 1, 0, 3, 6 };
+int temp, Num = 786; 
+object Ob;
+
+try
+{
+    sarray[10] = 2;
+    
+    
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+try
+{
+    temp = sarray[0] / sarray[1];
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+try
+{
+    Ob = Num;
+    int num = (short)Ob;
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+
+
+
+ 
